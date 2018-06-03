9a0a9dc79f1daa900b3c41032d049a5e5aee1993
RohanBh <brohan52@gmail.com>
2018-06-04 00:19:55 +0530

    Add lightning trails on still Flash

--- src/script.js
+++ src/script.js
@@ -12,12 +12,12 @@ flash.rotate(30);
 
 var speed = 40;
 var flashLen = flash.length;
-var dir = 120;
+var dirn = 30;
+var dirDelta = 60;
 
 points = chooseRandomPoints(rect);
-for (i = 0; i < points.length; i++) {
-    var path = new Path(points[i]);
-    path.selected = true;
+for (j = 0; j < points.length; j++) {
+    drawTrail(points[j]);
 }
 
 function chooseRandomPoints(flashBounds) {
@@ -29,5 +29,28 @@ function chooseRandomPoints(flashBounds) {
 	pts.push(pt.rotate(30,flashBounds.center));
     }
     return pts;
+}
+
+function drawTrail(point) {
+    var trailPts = trailLength();
+    var lastPt = point;
+    var trail = new Path(point);
+    for (i = 0; i < trailPts; i++) {
+	var step = Math.random() * 15;
+	var angle = getAngle();
+	var newPt = new Point({length: step, angle: angle});
+	trail.add(lastPt + newPt);
+	lastPt += newPt;
+    }
+    trail.strokeColor = 'white';
+}
+
+function getAngle() {
+    var sign = Math.pow(-1, Math.floor(Math.random() * 2));
+    return 180 + dirn + sign * Math.pow(Math.random(), 2) * dirDelta;
+}
+
+function trailLength() {
+    return Math.floor(Math.random()*20 + speed * 0.8);
 }
 
